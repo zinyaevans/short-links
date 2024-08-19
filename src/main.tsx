74622dd5bc17c4a436d6a28481b8a4be8d7c6d68
Nadya <prokopyevans@gmail.com>
2024-08-19 19:15:41 +0500

    feat: fsd architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "./app/App.tsx";
+import "./app/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
